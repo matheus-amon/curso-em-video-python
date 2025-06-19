963dc883dc7e8b337b30ed1379ab290ee73ba680
matheus-amon <matheus.amon@outlook.com>
2025-06-18 21:06:33 -0300

    ex054

---
+++ 2world-two/ex054.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+years: list = []
+for y in range(1, 8):
+    year = input(f'ano da {y}º pessoa: ')
+    year = int(year)
+    years.append(year)
+
+m: int = 0
+for y in years:
+    current_year = date.today().year
+    current_year = int(current_year)
+    age = current_year - y
+    
+    if age < 18:
+        m += 1
+        
+print(f'''
+{m} menores
+{len(years) - m} maiores
+''')
